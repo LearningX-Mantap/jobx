e94643f5a7c76e6296c59eacceec5bdc22578020
Iqbal LX <iqbal@alumni.pcr.ac.id>
2023-10-30 09:36:14 +0700

    navbar mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,19 +59,19 @@ $(document).ready(function(){
       'id': ''
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>');
+    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
     $('body').append('<div id="mobile-body-overly"></div>');
-    $('#mobile-nav').find('.menu-has-children').prepend('<i class="lnr lnr-chevron-down"></i>');
+    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
 
     $(document).on('click', '.menu-has-children i', function(e) {
       $(this).next().toggleClass('menu-item-active');
       $(this).nextAll('ul').eq(0).slideToggle();
-      $(this).toggleClass("lnr-chevron-up lnr-chevron-down");
+      $(this).toggleClass("fa fa-chevron-up fa fa-chevron-down");
     });
 
     $(document).on('click', '#mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
-      $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
+      $('#mobile-nav-toggle i').toggleClass('fa fa-times fa fa-bars');
       $('#mobile-body-overly').toggle();
     });
 
